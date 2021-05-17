feb249341d0c410c4552a4e20fc092ef00f5e7e2
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2021-05-18 00:19:08 +0200

    possible moves implemented

--- backgammon.js
+++ backgammon.js
@@ -12,6 +12,7 @@ const defaultPieces = {
         5, 0, 0, 0, 0, 0,  //top right
         5, 0, 0, 0, 0, 0,  //bot left
         0, 0, 0, 0, 0, 2,  //bot right
+        0,                 //home
         0                  //bar
     ],
     'white': [
@@ -19,9 +20,39 @@ const defaultPieces = {
         0, 0, 0, 0, 0, 2, //top right
         0, 0, 0, 0, 3, 0, //bot left
         5, 0, 0, 0, 0, 0, //bot right
+        0,                //home
         0                 //bar
     ]
 };
+
+/**
+     * Movement map
+     * White moves CW from idx 23->12 and then from 0->11
+     * Black moves CCW from idx 11->0 and then from 12->23
+     * Black home is right of 23
+     * Black exits bar to 11
+     * White home is right of 11
+     * White exits bar to 23
+     */
+const sequence = {
+    white: [
+        25, //bar
+        23, 22, 21, 20, 19, 18,
+        17, 16, 15, 14, 13, 12,
+        0, 1, 2, 3, 4, 5,
+        6, 7, 8, 9, 10, 11,
+        24 //home
+    ],
+    black: [
+        25, //bar
+        11, 10, 9, 8, 7, 6,
+        5, 4, 3, 2, 1, 0,
+        12, 13, 14, 15, 16, 17,
+        18, 19, 20, 21, 22, 23,
+        24 //home
+    ]
+};
+
 let pieces = JSON.parse(JSON.stringify(defaultPieces));
 let barCount = 0;
 let barWidth = 50;
@@ -49,16 +80,21 @@ function drawBoard() {
 
     const boardCanvas = document.getElementById('bgBoard');
     const pieceCanvas = document.getElementById('bgPieceLayer');
+    const movesCanvas = document.getElementById('possibleMovesLayer');
     boardCanvas.style.width = w + "px";
     boardCanvas.style.height = h + "px";
     pieceCanvas.style.width = w + "px";
     pieceCanvas.style.height = h + "px";
+    movesCanvas.style.width = w + "px";
+    movesCanvas.style.height = h + "px";
 
     // Set actual size in memory (scaled to account for extra pixel density).
     boardCanvas.width = scaledWidth;
     boardCanvas.height = scaledHeight;
     pieceCanvas.width = scaledWidth;
     pieceCanvas.height = scaledHeight;
+    movesCanvas.width = scaledWidth;
+    movesCanvas.height = scaledHeight;
 
     /****** Draw board *******/
 
@@ -128,7 +164,7 @@ function drawPieces() {
                     if (place == 'bar') {
                         barCount++;
                     }
-                    drawPiece(col, place, offset, pcCtr);
+                    drawPiece('bgPieceLayer', col, place, offset, pcCtr);
                 }
             }
         }
@@ -141,8 +177,8 @@ function drawPieces() {
  * idx = index position (void in case place = 'bar') 
  * num = piece number at this position
  */
-function drawPiece(color, place, idx, num) {
-    const ctx = document.getElementById('bgPieceLayer').getContext('2d');
+function drawPiece(layer, color, place, idx, num) {
+    const ctx = document.getElementById(layer).getContext('2d');
     let numDraw = num;
     let x = Math.floor(pointWidth * idx + pieceRadius + (pointWidth - (2.25 * pieceRadius)));
     let y = pieceRadius;
@@ -237,15 +273,105 @@ document.getElementById('bgPieceLayer').addEventListener("click", function (e) {
     if (y > scaledHeight / 2) {
         estimatedOffset += 12;
     }
-    console.log('offset: ' + estimatedOffset);
-    console.log(pieces[color][estimatedOffset] + ' ' + color + ' pieces')
+    console.log('offset: ' + estimatedOffset + ' ' + pieces[color][estimatedOffset] + ' ' + color + ' pieces');
+    showMoves(color, estimatedOffset);
 });
 
-function showMoves() {
+// add event handler to see if we have clicked a game piece
+document.getElementById('possibleMovesLayer').addEventListener("click", function (e) {
+    var pos = getPosition(this);
+    var x = e.pageX - pos.x;
+    var y = e.pageY - pos.y;
+    var coord = "x=" + x + ", y=" + y;
+    var c = this.getContext('2d');
+    var p = c.getImageData(x, y, 1, 1).data;
+    var col = p[0] + p[1] + p[2];
+    var color = 'unknown';
+
+    // did we click a game piece (color != transparent)
+    if (col + p[3] == 0) {
+        hideMoves();
+        return false;
+    } else {
+        let barW = 0;
+        if (x > scaledWidth / 2) {
+            barW = barWidth;
+        }
+        console.log(p[0] + ' ' + p[1] + ' ' + p[2] + " " + color + " " + coord);
+        let estimatedOffset = Math.floor((x - barW) / pointWidth);
+        if (y > scaledHeight / 2) {
+            estimatedOffset += 12;
+        }
+        console.log('offset: ' + estimatedOffset + ' ' + pieces[color][estimatedOffset] + ' ' + color + ' pieces');
+        //TODO: move piece and call drawPieces()
+    }
+});
+
+function hideMoves() {
+    let movesLayer = document.getElementById('possibleMovesLayer');
+    if (!movesLayer.classList.contains('hidden')) {
+        movesLayer.classList.toggle('hidden');
+    }
+    let ctx = movesLayer.getContext('2d');
+    ctx.clearRect(0, 0, movesLayer.width, movesLayer.height);
+}
+
+function showMoves(color, idx) {
+    let d1 = parseInt(document.getElementById('d1').value);
+    let d2 = parseInt(document.getElementById('d2').value);
     let movesLayer = document.getElementById('possibleMovesLayer');
     let currColor = ((document.getElementById('white').checked) ? 'white' : 'black');
-    movesLayer.classList.toggle('hidden');
+    let oppositeColor = ((document.getElementById('white').checked) ? 'black' : 'white');
+    // if color isn't current, exit
+    if (color != currColor) {
+        console.error("not your turn");
+        return false;
+    }
+    if (movesLayer.classList.contains('hidden')) {
+        movesLayer.classList.toggle('hidden');
+    }
     console.log('current player = ' + currColor);
+    let possibleMoves = [
+        false, false, false, false, false, false, //top left
+        false, false, false, false, false, false, //top right
+        false, false, false, false, false, false, //bot left
+        false, false, false, false, false, false, //bot right
+        false //home
+    ];
+
+    if (pieces[currColor][idx] > 0) {
+        let currspot = sequence[currColor].indexOf(idx);
+        //console.log("Found " + pieces[currColor][idx] + " " + currColor + " pieces at " + idx + " next field is " + sequence[currColor][currspot + 1]);
+
+        //TODO: disallow move to 25 (home) if not all pieces are in home quarter
+
+        //check if opposite color is present on points identified by die 1
+        if (pieces[oppositeColor][sequence[currColor][currspot + d1]] <= 1) {
+            possibleMoves[sequence[currColor][currspot + d1]] = true;
+        }
+        //check if opposite color is present on points identified by die 2
+        if (pieces[oppositeColor][sequence[currColor][currspot + d2]] <= 1) {
+            possibleMoves[sequence[currColor][currspot + d2]] = true;
+        }
+        //check if opposite color is present on points identified by sum of both dice
+        if (pieces[oppositeColor][sequence[currColor][currspot + d1 + d2]] <= 1) {
+            possibleMoves[sequence[currColor][currspot + d1 + d2]] = true;
+        }
+
+        for (let z = 0; z < possibleMoves.length; z++) {
+            let place = 'top';
+            let off = z;
+            if (z > 11) {
+                off = z - 12;
+                place = 'bot';
+            }
+            if (possibleMoves[z]) {
+                drawPiece('possibleMovesLayer', 'green', place, off, 1);
+            }
+        }
+
+    }
+    //console.log(possibleMoves);
 }
 
 
