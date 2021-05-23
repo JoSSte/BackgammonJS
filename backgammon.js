777a0d5bfc7bd946fe9d1c9e283e273df8d3fdc7
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2021-05-23 12:07:25 +0200

    current color and drag start implemented

--- backgammon.js
+++ backgammon.js
@@ -12,3 +12,54 @@ function setBoardDimensions() {
     board.style.width = width;
     board.style.height = height;
 }
+
+function handleDragStart(e) {
+    this.style.opacity = '0.4';
+}
+
+function handleDragEnd(e) {
+    this.style.opacity = '1';
+}
+
+let items = document.querySelectorAll('.point .pip');
+items.forEach(function (item) {
+    item.addEventListener('dragstart', handleDragStart, false);
+    item.addEventListener('dragend', handleDragEnd, false);
+});
+
+/**
+ * Set either black or white draggable
+ * @param {string} color 
+ */
+function setDraggable(color) {
+    let otherColor = 'black';
+    if (color == 'black') {
+        otherColor = 'white';
+    }
+    let pieces = document.querySelectorAll('.pip.' + color );
+    console.log('Found ' + pieces.length + ' ' + color + ' pieces');
+    let otherPieces = document.querySelectorAll('.pip.' + otherColor);
+    console.log('Found ' + otherPieces.length + ' ' + otherColor + ' pieces');
+    pieces.forEach(function (item) {
+        item.draggable = true;
+    });
+    otherPieces.forEach(function (item) {
+        item.draggable = false;
+    });
+}
+
+let currColor = 'none';
+let oppositeColor = 'none';
+
+function getCurrentColor(){
+    let prevColor = currColor;
+    currColor = ((document.getElementById('white').checked) ? 'white' : 'black');
+    oppositeColor = ((document.getElementById('white').checked) ? 'black': 'white');
+    if(currColor != prevColor){
+        setDraggable(currColor);
+    }
+    return currColor;
+}
+
+
+
